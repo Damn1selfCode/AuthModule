981e4932fad91cb8a8b8ac5fb4e5b81bb68dacf0
Damn1selfCode <112675720+Damn1selfCode@users.noreply.github.com>
2023-11-03 08:23:12 -0500

    Paypal Service

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\PlanSubs\PlanSubsController;
+use App\Services\PaypalService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class WelcomeController extends Controller
+{
+
+    public function index()
+    {
+        //no se autodestruye la cache hasta hacerlo manual
+        // $planesPaypal = Cache::rememberForever('keyPlanes', function () {
+        //     return $this->planes();
+        // });
+
+        $planesPaypal = Cache::remember('planes', 60, function () {
+            return $this->planes();
+        });
+        return view('welcome', compact('planesPaypal'));
+    }
+
+    private function planes()
+    {
+        $servicio  = new  PaypalService;
+        $data = $servicio->ListarPlanes();
+        $planesPaypal = $data['plans'];
+        return $planesPaypal;
+    }
+}
