f786f1be7978ddafd9b23cdcc21b53c260107b8d
Damn1selfCode <112675720+Damn1selfCode@users.noreply.github.com>
2023-11-03 08:10:51 -0500

    Migraciones db

--- database/migrations/2023_10_06_201030_create_plans_table.php
+++ database/migrations/2023_10_06_201030_create_plans_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('plans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug')->unique();
+            $table->integer('price')->unsigned();// sin decimales
+            $table->integer('duration_in_days');
             $table->timestamps();
-            //$table->softDeletes();
         });
     }
 
@@ -28,6 +25,11 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('plans');
     }
 };
+
+
+
+
+
