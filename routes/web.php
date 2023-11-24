1736f0819a65abc5c96a7a3d270101acd91a1cb2
MAac09 <yovanavera118@gmail.com>
2023-11-24 11:35:41 -0500

    ah
    ah

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
-use App\Http\Controllers\{
-    AcademyController,
-    ProfileController,
-    WelcomeController,
-    SoporteController,
-    SuscripcionController,
-};
+use App\Http\Controllers\AcademyController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\SoporteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,23 +21,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
-Route::get('/suscripcion', [SuscripcionController::class, 'index']);
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+/*
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::delete('/profile/deactivate/{user}', 'App\Http\Controllers\ProfileController@deactivate')->name('profile.deactivate');
-});
+});*/
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update.put'); // Agregada la ruta PUT
+    Route::delete('/profile/deactivate/{user}', 'App\Http\Controllers\ProfileController@deactivate')->name('profile.deactivate');
+});
 
 Route::post('/suscription/suscribirse', 'App\Http\Controllers\SubscriptionController@suscribirse')->name('suscription.suscribirse');
 Route::post('/suscription/desuscribirse', 'App\Http\Controllers\SubscriptionController@desuscribirse')->name('suscription.desuscribirse');
-
 
 require __DIR__ . '/auth.php';
 require __DIR__ . '/academia.php';
@@ -50,12 +51,8 @@ Route::get('/usuarios', function () {
     $plans = app(WelcomeController::class)->planes();
     $user = auth()->user(); // Obtener el usuario autenticado
     $suscripcion = ($user->suscripcion === null) ? 0 : $user->suscripcion->suscripcion;
-    $referido = app(SuscripcionController::class)->index($user->id);
-    dd($referido);
-    $codRef = ($user->CodRef === null) ? null : $user->CodRef;
-    $user->code = 'sadasd';
-    //DD($user);
-    return view('usuarios', compact('user', 'plans', 'suscripcion', 'codRef', 'referido'));
+    $user->code = hash('sha256', $user->email);
+    return view('usuarios', compact('user', 'plans', 'suscripcion'));
 })->middleware('verified')
     ->name('usuarios');
 
@@ -94,7 +91,3 @@ Route::get('/soporte/lectura-ticket/{id}/{origen?}', 'App\Http\Controllers\Sopor
 Route::post('/soporte/lectura-ticket/{id}', 'App\Http\Controllers\SoporteController@lecturaTicket');
 Route::post('/soporte/enviar-a-papelera', 'App\Http\Controllers\SoporteController@enviarAPapelera')->name('soporte.enviar-a-papelera');
 Route::post('/soporte/recuperar-de-papelera', 'App\Http\Controllers\SoporteController@recuperarDePapelera')->name('soporte.recuperar-de-papelera');
-
-
-Route::post('/CodigoRefererido/generar', 'App\Http\Controllers\CodigoRefereridoController@generar_CodigoReferido')->name('codigo.generar');
-Route::post('/CodigoRefererido/actualizar', 'App\Http\Controllers\CodigoRefereridoController@actualizar_CodigoReferido')->name('codigo.actualizar');
